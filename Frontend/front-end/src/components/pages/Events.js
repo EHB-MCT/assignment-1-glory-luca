6811ace1b328ebe9d25dfffe89974a98b043dd27
GloryMenga <glory.menga@student.ehb.be>
2024-10-24 15:13:54 +0200

    Nav for the events and home page done

---
+++ Frontend/front-end/src/components/pages/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className='events'>
+      <h1>Welcome to the Events Page</h1>
+    </div>
+  );
+};
+
+export default Events;
