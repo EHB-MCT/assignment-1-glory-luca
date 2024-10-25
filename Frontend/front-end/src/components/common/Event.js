136649e56709d17cfca2627a176236f9054f2fea
GloryMenga <glory.menga@student.ehb.be>
2024-10-25 18:49:02 +0200

    Dummy data done for the event

---
+++ Frontend/front-end/src/components/common/Event.js
@@ -0,0 +1,18 @@
+function Event() {
+    return (
+      <>
+        <div className="cards">
+            <div className="card">
+                <h1>Meetup</h1>
+                <p>a little meet up</p>
+                <div className="date-time">
+                    <h2>25-10-2024</h2>
+                    <h2>3:00 AM</h2>
+                </div>
+            </div>                
+        </div>
+      </>
+    );
+  }
+  
+  export default Event;
--- Frontend/front-end/src/components/pages/Events.js
+++ Frontend/front-end/src/components/pages/Events.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Event from '../common/Event.js';
 
 const Events = () => {
   return (
     <div className='events'>
       <h1>See your events:</h1>
+      <Event/>
     </div>
   );
 };
